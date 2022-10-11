01e5d4743f9a25a4d97e05adc29cec5ee24e1772
キャンド <amehikoji@gmail.com>
2022-10-11 16:15:29 -0400

    clarify comment

--- grtests/main.rs
+++ grtests/main.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), String> {
     utils::LayoutBuilder::two_dimension()
         .real_real((800, 600))
         .add_linear_function(|s| {
-            // from, to (ms)
+            // [from, to) (ms)
             s.add_snapshot_from_slope_form(0, 300, 0., 0., 3.)
             .add_snapshot_from_slope_form(299, 600, 0.005, 0., 3.)
             .add_snapshot_from_slope_form(599, 900, 0.01, 0., 3.)
