ec01667d027694e89a696e89416c74fc47e61b81
井澤伊織 <izawa.iori.tan@gmail.com>
2022-10-13 13:30:23 -0400

    add more utils

--- grtests/main.rs
+++ grtests/main.rs
@@ -1,11 +1,11 @@
-use matan::utils;
+use matan;
 
 fn main() -> Result<(), String> {
-    utils::LayoutBuilder::two_dimension()
+    matan::utils::LayoutBuilder::two_dimension()
         .real_real((800, 600))
         .add_linear_function(|s| {
             // [from, to) (ms)
-            s.add_snapshot_from_slope_form(0, 3000, 3./4., 0., 3.)
+            s.add_transition_from_slope_form(0, 3000, (3./4., 0., 3.), (1., 0., 3.), 100)
         })
         .run("Test")?;
     Ok(())
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -47,6 +47,28 @@ impl LinearFunctionBuilder {
         self
     }
 
+    pub fn add_snapshots_from_slope_form(mut self, from: u64, to: u64, parts: &[(f32, f32, f32)]) -> Self {
+        let dv = (((to-from) as usize)/parts.len()) as u64;
+        for i in 0..parts.len() {
+            let cs = (dv*((i+1) as u64), dv*((i+2) as u64));
+            let n = parts[i];
+            self.f.push((cs.0, cs.1, linear::FunctionDescriptor::Angle(linear::AngularDescriptor::new(n.0, n.1, n.2))));
+        }
+        self
+    }
+
+    pub fn add_transition_from_slope_form(self, from: u64, to: u64, start: (f32, f32, f32), end: (f32, f32, f32), divs: usize) -> Self {
+        let k = divs as f32;
+        let n = ((end.0-start.0)/k, (end.1-start.1)/k, (end.2-start.2)/k);
+        let mut  parts = Vec::new();
+        for i in 0..(divs+1) {
+            let m = i as f32;
+            parts.push((start.0 + n.0*m, start.1 + n.1*m, start.2 + n.2*m));
+        }
+        println!("{:?}", &parts);
+        self.add_snapshots_from_slope_form(from, to, &parts)
+    }
+
     fn build(&self, screen_size: (u32, u32)) -> (Vec<(u64, u64)>, linear::Function) {
         let mut n = linear::Function::new(screen_size);
         for (s, e, d) in &self.f {
